739a44719b30b3cb31b4edc2fbf97374d8fe375f
Ruveyda Kayabasi <rvyd.kybs@outlook.com>
2023-01-29 13:00:23 +0200

    fetch pokedex data

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [pokemons, setPokemon] = useState({
+    pokeItems: [],
+    isDataLoaded: false,
+  });
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
+      .then((res) => res.json())
+      .then((json) => {
+        setPokemon({ pokeItems: json, isDataLoaded: true });
+      })
+      .then((data) => console.log("data", data));
+  }, []);
+  console.log("first", pokemons);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <body>
+        <a>fd</a>
+      </body>
     </div>
   );
 }
