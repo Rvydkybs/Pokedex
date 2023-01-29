aa8ea96ad9e800a35ea3ed42428342341236aab8
Ruveyda Kayabasi <rvyd.kybs@outlook.com>
2023-01-29 14:00:26 +0200

    fetch images in png format

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -1,25 +1,52 @@
 import "./App.css";
 import React, { useEffect, useState } from "react";
 
+const { name, url } = "https://pokeapi.co/api/v2/pokemon?limit=150";
+let pokemonIndex = "";
+
 function App() {
   const [pokemons, setPokemon] = useState({
     pokeItems: [],
     isDataLoaded: false,
+    pokemonIndex: "",
   });
   useEffect(() => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
       .then((res) => res.json())
       .then((json) => {
         setPokemon({ pokeItems: json, isDataLoaded: true });
-      })
-      .then((data) => console.log("data", data));
+      });
+    pokemonIndex = "https://pokeapi.co/api/v2/pokemon?limit=150".split("/")[
+      "https://pokeapi.co/api/v2/pokemon?limit=150".split("/").length - 2
+    ];
+    {
+      console.log("fkkkkkkkkkkkkkkkkkkkkkkkk", pokemonIndex);
+    }
   }, []);
   console.log("first", pokemons);
   return (
-    <div className="App">
-      <body>
-        <a>fd</a>
-      </body>
+    <div
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      {pokemons.pokeItems.results &&
+        pokemons.pokeItems.results.map((item, index) => {
+          return (
+            <div className="container">
+              <div className="textContainer">
+                <a style={{ backgroundColor: "blue" }}>{item.name}</a>
+              </div>
+              <div className="imageContainer">
+                <img
+                  style={{ backgroundColor: "pink" }}
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index}.png`}
+                />
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
