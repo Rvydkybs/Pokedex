7bf6b72364c70efbf865a0917c058eb37d5b0a9a
Ruveyda Kayabasi <rvyd.kybs@outlook.com>
2023-02-04 15:12:26 +0200

    use useFetch Hook for fetch data back-up

--- pokedex/src/Hooks/useFetch.js
+++ pokedex/src/Hooks/useFetch.js
@@ -1,1 +1,17 @@
+import React, { useEffect, useState, useMemo } from "react";
 
+export default function useFetch() {
+  const [pokemons, setPokemon] = useState({
+    pokeItems: [],
+    isDataLoaded: false,
+  });
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=150")
+      .then((res) => res.json())
+      .then((json) => {
+        setPokemon({ pokeItems: json, isDataLoaded: true });
+      });
+    console.log("deneme", pokemons.pokeItems);
+  }, []);
+  return pokemons;
+}
